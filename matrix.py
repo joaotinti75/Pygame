1968c3a7d0e529dace4986a29206226cc22c592b
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2020-09-05 20:19:10 -0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,13 +1,16 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import choice, randint
+from random import choice, randrange
 
 pygame.init()
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-FONT_SIZE = 10
+FONT_SIZE = 20
+WHITE = (255,255,255)
+GREEN = (0, 160, 0)
+FPS = 15
 generate = False
 
 letter = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','x','y','z']
@@ -18,59 +21,95 @@ counter = 1
 
 fonte = pygame.font.SysFont('arial', FONT_SIZE, False, True)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
+pygame.display.set_caption('Matrix')
 
 class Codeline:
     def __init__(self):
         self.lista_ypos = []
         self.lista_letters = []
-        self.xpos = randint(FONT_SIZE, SCREEN_WIDTH-FONT_SIZE) 
-        self.ypos = randint(0, SCREEN_HEIGHT-FONT_SIZE)
+        self.xpos = randrange(FONT_SIZE, SCREEN_WIDTH + FONT_SIZE, FONT_SIZE) 
+        self.ypos = randrange(0, SCREEN_HEIGHT+FONT_SIZE, FONT_SIZE)
         self.lista_ypos.append(self.ypos)
-
-    def draw_codeline(self):
+        
+    def draw_codeline(self): #method that draws letters on sreen
         randomic_letter = choice(letter)
         self.lista_letters.append(randomic_letter)
 
         for msg, y in zip(self.lista_letters, self.lista_ypos):
-            message = f'{msg}'
-            text = fonte.render(message, True, (0,160,0))
-            screen.blit(text, (self.xpos, y))
+            if self.lista_letters.index(msg) == len(self.lista_letters) - 1:
+                if self.lista_letters.count(msg) < 2:
+                    message = f'{msg}'
+                    text = fonte.render(message, True, WHITE)
+                    screen.blit(text, (self.xpos, y))
+                else:
+                    message = f'{msg}'
+                    text = fonte.render(self.lista_letters[-1], True, WHITE)
+                    screen.blit(text, (self.xpos, y))
+                    for i in range(self.lista_letters[:-1].count(msg)):
+                        message = f'{msg}'
+                        text = fonte.render(message, True, GREEN)
+                        screen.blit(text, (self.xpos, y))
+            else:
+                    message = f'{msg}'
+                    text = fonte.render(message, True, GREEN)
+                    screen.blit(text, (self.xpos, y))
+    
+        '''for msg, y in zip(self.lista_letters, self.lista_ypos):
+            if self.lista_letters[-1] == msg:
+                if msg in self.lista_letters[:-1]:
+                    message = f'{msg}'
+                    text = fonte.render(message, True, GREEN)
+                    screen.blit(text, (self.xpos, y))
+                else:
+                    message = f'{msg}'
+                    text = fonte.render(message, True, WHITE)
+                    screen.blit(text, (self.xpos, y))
+            else:
+                message = f'{msg}'
+                text = fonte.render(message, True, GREEN)
+                screen.blit(text, (self.xpos, y))'''
 
-    def desloc_codeline(self):
-        self.ypos += FONT_SIZE - 6
+    def desloc_codeline(self): #method that moves the letters on screen
+        self.ypos += FONT_SIZE - 6 
         self.lista_ypos.append(self.ypos)
 
 obj_list = []
+repeat_list = []
 '''
 for i in range(200):
     obj = Codeline()
     obj_list.append(obj)'''
 
 def insert_obj(obj_list, generate):
-    if generate:
-        for i in range(1):
+    if generate: #if generate is True, the for loop will continuously create new objects
+        for i in range(10): #creating objects and saving them in a list
             obj = Codeline()
-            obj_list.append(obj)
-        for i in obj_list:
+            if obj.xpos in repeat_list or obj.ypos > SCREEN_HEIGHT:
+                del obj
+            else:
+                repeat_list.append(obj.xpos)
+                obj_list.append(obj)
+            
+        for i in obj_list: #drawing the objects and moving them on the screen
             i.draw_codeline()
             i.desloc_codeline()
-    else:
+    else: #if generate is False new objects won't be created anymore
         for i in obj_list:
-            i.draw_codeline()
+            i.draw_codeline()   
             i.desloc_codeline()
 
 clock = pygame.time.Clock()
 
 while True:
     screen.fill((0,0,0))
-    clock.tick(18)
+    clock.tick(FPS)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         
-    if counter < 10:    
+    if counter < 20:    
         if current_time - initial_time > 1000:
             generate = True
             initial_time = pygame.time.get_ticks()
