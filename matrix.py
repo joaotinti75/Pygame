9504958ce3614b52fcac84dfc8a463e0416354e4
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2020-09-09 21:18:33 -0300

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,121 +1,120 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random import choice, randrange
+from random import randrange, choice
 
 pygame.init()
 
-SCREEN_WIDTH = 540
-SCREEN_HEIGHT = 480
-FONT_SIZE = 8
-WHITE = (255, 255, 255)
-GREEN = (0, 200, 0)
-BLACK = (0, 0, 0)
-RED = (200, 0, 0)
-BLUE = (0, 0, 200)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
 COLORS = [GREEN, RED, BLUE]
 color_change = 0
 
-FPS = 12
+FONT_SIZE = 8
+
+FPS = 15
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+obj_list = []
+repeat_list = []
+number_of_obj = 20
 generate = False
 
-letter = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','x','y','z']
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','0','1','2','3','4','5','6','7','8','9']
 
-initial_time = 0
-current_time = 0
-color_time = 0
-counter = 20
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Matrix Animation')
 
-fonte = pygame.font.SysFont('arial', FONT_SIZE, True, True)
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
-pygame.display.set_caption('Matrix')
+font = pygame.font.SysFont('arial', FONT_SIZE, True, True)
+#message = 'hello world'
+#text = font.render(message, True, GREEN)
 
 class Codeline:
     def __init__(self):
         self.color = GREEN
         self.fill = 1
-        self.lista_ypos = []
-        self.lista_letters = []
-        self.xpos = randrange(FONT_SIZE, SCREEN_WIDTH + FONT_SIZE, FONT_SIZE) 
+        self.ypos_list = []
+        self.letters_list = []
+        self.xpos = randrange(FONT_SIZE, SCREEN_WIDTH + FONT_SIZE, FONT_SIZE)
         self.ypos = randrange(0, SCREEN_HEIGHT+FONT_SIZE, FONT_SIZE)
-        self.lista_ypos.append(self.ypos)
-        
-    def draw_codeline(self): #method that draws letters on sreen
-        randomic_letter = choice(letter)
-        self.lista_letters.append(randomic_letter)
+        self.ypos_list.append(self.ypos)
 
-        for msg, y in zip(self.lista_letters, self.lista_ypos):
+    def draw_codeline(self):
+        randomic_letter = choice(letters)
+        self.letters_list.append(randomic_letter)
+
+        for msg, y in zip(self.letters_list, self.ypos_list):
             message = f'{msg}'
-            text = fonte.render(message, True, self.color)
-            #screen.fill(BLACK, (self.xpos, y, text.get_width() + FONT_SIZE, text.get_height()))
+            text = font.render(message, True, self.color)
             screen.blit(text, (self.xpos, y))
 
-    def desloc_codeline(self): #method that moves the letters on screen
-        if self.ypos > SCREEN_HEIGHT or len(self.lista_ypos) > 20:
+    def desloc_codeline(self):
+        if self.ypos > SCREEN_HEIGHT or len(self.ypos_list) > 20:
             message = ' '
-            text = fonte.render(message, True, WHITE)
-            screen.fill(BLACK, (self.xpos, self.lista_ypos[0], text.get_width() + FONT_SIZE, text.get_height()*self.fill))
-            if text.get_height() * self.fill > len(self.lista_ypos) * text.get_height():
-                self.lista_letters.clear()
+            text = font.render(message, True, WHITE)
+            screen.fill(BLACK, (self.xpos, self.ypos_list[0], text.get_width() + FONT_SIZE, text.get_height()* self.fill))
+            if text.get_height() * self.fill > len(self.ypos_list) * text.get_height():
+                self.letters_list.clear()
             else:
                 self.fill += 1
 
         else:
-            self.ypos += FONT_SIZE 
-            self.lista_ypos.append(self.ypos)
-            message = f'{self.lista_letters[-1]}'
-            text = fonte.render(message, True, WHITE)
-            screen.fill(BLACK, (self.xpos, self.lista_ypos[-1], text.get_width() + FONT_SIZE, text.get_height()))
-            screen.blit(text, (self.xpos, self.lista_ypos[-1]))
+            self.ypos += FONT_SIZE
+            self.ypos_list.append(self.ypos)
+            message = f'{self.letters_list[-1]}'
+            text = font.render(message, True, WHITE)
+            screen.fill(BLACK, (self.xpos, self.ypos_list[-1], text.get_width()+FONT_SIZE, text.get_height()))
+            screen.blit(text, (self.xpos, self.ypos_list[-1]))
 
-obj_list = []
-repeat_list = []
-
-def insert_obj(obj_list, generate, counter, color_change):
-    if generate: #if generate is True, the for loop will continuously create new objects
-        for i in range(counter): #creating objects and saving them in a list
+def create_multiples_obj(obj_list, number_of_obj, generate):
+    if generate:
+        for i in range(number_of_obj):
             obj = Codeline()
             if obj.xpos in repeat_list or obj.ypos > SCREEN_HEIGHT:
                 repeat_list.remove(obj.xpos)
-                obj.lista_letters.clear()
+                obj.letters_list.clear()
                 del obj
-                #pass
             else:
-                repeat_list.append(obj.xpos)
                 obj_list.append(obj)
-            
-        for i in obj_list: #drawing the objects and moving them on the screen
+                repeat_list.append(obj.xpos)
+        for i in obj_list:
             i.color = COLORS[color_change]
             i.draw_codeline()
             i.desloc_codeline()
-            
-    else: #if generate is False new objects won't be created anymore
+    else:
         for i in obj_list:
             i.color = COLORS[color_change]
-            i.draw_codeline()   
+            i.draw_codeline()
             i.desloc_codeline()
 
 clock = pygame.time.Clock()
+initial_time = 0
+current_time = 0
+color_time = 0
 
 while True:
-    screen.fill((0,0,0))
     clock.tick(FPS)
-
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    
+
     current_time = pygame.time.get_ticks()
 
-    insert_obj(obj_list, generate, counter, color_change)
-
-    if current_time - color_time > 3000:
+    if current_time - color_time > 10000: #10 seconds
         color_change = (color_change + 1) % len(COLORS)
         color_time = pygame.time.get_ticks()
 
-    if current_time - initial_time > 200:
+    if current_time - initial_time > 200: #0.2 seconds
         generate = True
         initial_time = pygame.time.get_ticks()
+
+    create_multiples_obj(obj_list, number_of_obj, generate)
 
     pygame.display.flip()
