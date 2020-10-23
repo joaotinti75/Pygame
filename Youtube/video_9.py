5838393e4c4c10034f9bd4cde6badfcd02ea17a1
João Tinti <55903736+joaotinti75@users.noreply.github.com>
2020-10-23 11:07:05 -0300

    Create video_9.py

---
+++ Youtube/video_9.py
@@ -0,0 +1,110 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+from random import randint
+
+pygame.init()
+
+pygame.mixer.music.set_volume(0.1)
+musica_de_fundo = pygame.mixer.music.load('BoxCat_Games_-_08_-_CPU_Talk (1).mp3')
+pygame.mixer.music.play(-1)
+
+barulho_colisao = pygame.mixer.Sound('smw_coin (1).wav')
+
+largura = 640
+altura = 480
+
+x_cobra = int(largura/2) 
+y_cobra = int(altura/2)
+
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
+
+
+x_maca = randint(40, 600)
+y_maca = randint(50, 430)
+
+pontos = 0
+fonte = pygame.font.SysFont('arial', 40, bold=True, italic=True)
+
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Jogo')
+relogio = pygame.time.Clock()
+lista_cobra = []
+comprimento_inicial = 5
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        #XeY = [x, y]
+        #XeY[0] = x
+        #XeY[1] = y
+
+        pygame.draw.rect(tela, (0,255,0), (XeY[0], XeY[1], 20, 20))
+
+while True:
+    relogio.tick(30)
+    tela.fill((255,255,255))
+    
+    mensagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(mensagem, True, (0,0,0))
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle = velocidade
+                    x_controle = 0
+
+    x_cobra = x_cobra + x_controle
+    y_cobra = y_cobra + y_controle
+        
+    cobra = pygame.draw.rect(tela, (0,255,0), (x_cobra,y_cobra,20,20))
+    maca = pygame.draw.rect(tela, (255,0,0), (x_maca,y_maca,20,20))
+    
+    if cobra.colliderect(maca):
+        x_maca = randint(40, 600)
+        y_maca = randint(50, 430)
+        pontos += 1
+        barulho_colisao.play()
+        comprimento_inicial = comprimento_inicial + 1
+
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    
+    lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+
+    aumenta_cobra(lista_cobra)
+
+    tela.blit(texto_formatado, (450,40))
+
+    
+    pygame.display.update()
